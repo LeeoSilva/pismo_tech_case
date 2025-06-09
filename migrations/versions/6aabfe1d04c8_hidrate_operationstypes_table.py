907a3836d764c9bd70d6bbb0555d7c41798841d0
LeeoSilva <leeosilva0909@gmail.com>
2025-06-09 16:56:11 -0300

    Migrations are now running successfully.

--- migrations/versions/6aabfe1d04c8_hidrate_operationstypes_table.py
+++ migrations/versions/6aabfe1d04c8_hidrate_operationstypes_table.py
@@ -7,6 +7,11 @@ Create Date: 2025-06-09 11:39:50.886962
 """
 
 from typing import Sequence, Union
+import datetime
+
+from alembic import op
+import sqlmodel as sm
+import slugify
 
 
 # revision identifiers, used by Alembic.
@@ -14,6 +19,7 @@ revision: str = "6aabfe1d04c8"
 down_revision: Union[str, None] = "4d8e0de1b49d"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
+table_name: str = "operation_types"
 
 data = [
     "Normal Purchase",
@@ -24,16 +30,27 @@ data = [
 
 
 def upgrade() -> None:
-    pass
-    # for description in data:
-    #     operation_type = OperationType(
-    #         description=description,
-    #         slug=slugify.slugify(description, separator="_"),
-    #         created_at=str(datetime.datetime.now(datetime.timezone.utc)),
-    #     )
-    #     operation_type.save()
+    conn = op.get_bind()
+
+    rows = [
+        {
+            "description": description,
+            "slug": slugify.slugify(description),
+            "created_at": datetime.datetime.now(datetime.timezone.utc),
+        }
+        for description in data
+    ]
+
+    conn.execute(
+        sm.text(
+            f"""
+                INSERT INTO {table_name} (description, slug, created_at)
+                VALUES (:description, :slug, :created_at)
+            """
+        ),
+        rows,
+    )
 
 
 def downgrade() -> None:
-    pass
-    # op.execute(f"DELETE FROM {OperationType.__tablename__}")
+    op.execute(f"DELETE FROM {table_name}")
